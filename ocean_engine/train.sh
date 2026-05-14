e6091680427a1626daa3e3d90315a1978337705c
Yuan Gao <114328932+YuanGao-YG@users.noreply.github.com>
2026-05-14 20:58:16 +0800

    Add files via upload

---
+++ ocean_engine/train.sh
@@ -0,0 +1,33 @@
+wandb_group='PnP-Corrector'
+yaml_config='config/Model.yaml'
+config='DSLCast'
+batch_size=16
+run_num=$(date "+%Y%m%d-%H%M%S")
+multi_steps_finetune=1
+finetune_max_epochs=0
+TRAIN_DIR=$(dirname $(realpath train.py))
+
+export MASTER_ADDR=11.215.117.234 
+export MASTER_PORT=34119
+export WORLD_SIZE=16
+export NODE_RANK=0 
+
+# 设置NCCL环境变量
+source ~/.bashrc
+conda activate triton_v2
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+nohup torchrun --nproc_per_node=8 --nnodes=2 --node_rank=$NODE_RANK --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT train.py\
+  --yaml_config=$yaml_config --config=$config --run_num=$run_num --batch_size=$batch_size --multi_steps_finetune=$multi_steps_finetune --finetune_max_epochs=$finetune_max_epochs \
+  >> ./logs/${config}_${wandb_group}_rank0_${SLURM_JOB_ID}_${run_num}.log 2>&1 &
+
+ssh root@11.218.8.194 "
+source ~/.bashrc; \
+conda activate triton_v2; \
+cd $TRAIN_DIR; \
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7; \
+export MASTER_ADDR=$MASTER_ADDR; export MASTER_PORT=$MASTER_PORT; export WORLD_SIZE=16; export NODE_RANK=1; \
+nohup torchrun --nproc_per_node=8 --nnodes=2 --node_rank=1 --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT $TRAIN_DIR/train.py \
+  --yaml_config=$yaml_config --config=$config --run_num=$run_num --batch_size=$batch_size --multi_steps_finetune=$multi_steps_finetune --finetune_max_epochs=$finetune_max_epochs \
+>> $TRAIN_DIR/logs/${config}_${wandb_group}_rank1_${SLURM_JOB_ID}_${run_num}.log 2>&1 &"
